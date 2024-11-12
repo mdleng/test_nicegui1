24839f0335970fe2396ad626245293da20e7d7d1
mdleng <47358420+mdleng@users.noreply.github.com>
2024-11-12 21:36:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from nicegui import app, ui
 
-app.add_static_files('/stl', 'static')
+#app.add_static_files('/stl', 'static')
 
 with ui.scene(width=1024, height=800) as scene:
     scene.spot_light(distance=100, intensity=0.1).move(-10, 0, 10)
