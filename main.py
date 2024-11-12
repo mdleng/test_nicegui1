1a0b7f5e49986b85d4c807e70e58321a8c6b3787
mdleng <47358420+mdleng@users.noreply.github.com>
2024-11-12 21:32:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app.add_static_files('/stl', 'static')
 
 with ui.scene(width=1024, height=800) as scene:
     scene.spot_light(distance=100, intensity=0.1).move(-10, 0, 10)
-    scene.stl('/stl/pikachu.stl').move(x=-0.5).scale(0.06)
+    scene.stl('pikachu.stl').move(x=-0.5).scale(0.06)
 
 ui.run()
